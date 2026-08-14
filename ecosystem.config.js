871746d1f8b95c5da93a552c0925a1ef30eef2c5
giteshsingla2 <singitesh1@gmail.com>
2026-08-14 12:00:14 +0530

    chore: update ad-dashboard port from 4567 to 3005 in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,10 +17,10 @@ module.exports = {
       name: 'ad-dashboard',
       cwd: './apps/dashboard',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 4567',
+      args: 'start -p 3005',
       env: {
         NODE_ENV: 'production',
-        PORT: 4567,
+        PORT: 3005,
       },
       instances: 1,
       autorestart: true,
